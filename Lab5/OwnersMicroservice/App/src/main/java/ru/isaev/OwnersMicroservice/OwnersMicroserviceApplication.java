70105971a8b3e600077e9eb715d6a6910d268655
theSirD <isaev-daniel17@yandex.ru>
2024-05-09 15:54:24 +0300

    feat: created Controller, Service and Repository layers inside 3 microservices

---
+++ Lab5/OwnersMicroservice/App/src/main/java/ru/isaev/OwnersMicroservice/OwnersMicroserviceApplication.java
@@ -0,0 +1,12 @@
+package ru.isaev.OwnersMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwnersMicroserviceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OwnersMicroserviceApplication.class, args);
+    }
+
+}
