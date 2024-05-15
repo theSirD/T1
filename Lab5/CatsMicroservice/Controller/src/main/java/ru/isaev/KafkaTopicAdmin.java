1b319fe11f824d1fe67f4ef9b6b8fd85e4c61b98
theSirD <isaev-daniel17@yandex.ru>
2024-05-15 18:27:26 +0300

    fix: get rid of unused topic

--- Lab5/CatsMicroservice/Controller/src/main/java/ru/isaev/KafkaTopicAdmin.java
+++ Lab5/CatsMicroservice/Controller/src/main/java/ru/isaev/KafkaTopicAdmin.java
@@ -8,17 +8,7 @@ import org.springframework.kafka.config.TopicBuilder;
 @Configuration
 public class KafkaTopicAdmin {
     @Bean
-    public NewTopic getCatIdResponseTopic() {
-        return TopicBuilder.name("topic-cat-id-response").build();
-    }
-
-    @Bean
     public NewTopic getCatResponseTopic() {
         return TopicBuilder.name("topic-cat-response").build();
-    }
-
-    @Bean
-    public NewTopic getListOfCatsResponseTopic() {
-        return TopicBuilder.name("topic-list-of-cats-response").build();
     }
 }
